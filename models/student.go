11916de644f848e13a34a5c129e8148aa1710ab1
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-21 13:39:08 -0400

    created handlers for getting data from the db

---
+++ models/student.go
@@ -0,0 +1,65 @@
+package models
+
+import "github.com/Jeff-Marm-Al/student-registration/db"
+
+type Student struct {
+	StudentID int64
+	Firstname string `binding:"required"`
+	Lastname  string `binding:"required"`
+	Email     string `binding:"required"`
+	Password  string `binding:"required"`
+}
+
+// function to create students
+func (new *Student) Save() error {
+	// created sql query
+	query := "INSERT INTO students(first_name, last_name, email, password) VALUES (?, ?, ?, ?)"
+	statement, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	statementResult, err := statement.Exec(new.Firstname, new.Lastname, new.Email, new.Password)
+
+	if err != nil {
+		return err
+	}
+
+	studentID, err := statementResult.LastInsertId()
+
+	new.StudentID = studentID
+
+	return err
+}
+
+// function to list all students
+func GetAllStudents() ([]Student, error) {
+	// querying the database for all students
+	query := "SELECT * FROM students"
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var students []Student
+
+	for rows.Next() {
+		// new student struct for each student
+		var student Student
+		err := rows.Scan(&student.StudentID, &student.Firstname, &student.Lastname, &student.Email, &student.Password)
+
+		if err != nil {
+			return nil, err
+		}
+
+		students = append(students, student)
+	}
+
+	return students, nil
+}

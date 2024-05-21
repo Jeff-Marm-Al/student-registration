7b15202243d21947fdaa1c0f876fa7d364d0a96e
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-21 13:37:06 -0400

    added registered routes

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/Jeff-Marm-Al/student-registration/db"
+	"github.com/Jeff-Marm-Al/student-registration/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
+	routes.RegisterRoutes(server)
 	server.Run(":8080") // this is localhost:8080
 }

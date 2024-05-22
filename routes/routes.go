54ae4fd52208c3e0e25ed861f27aa2122230aa7b
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-22 13:28:50 -0400

    added api calls for creating a course and listing courses

--- routes/routes.go
+++ routes/routes.go
@@ -5,4 +5,6 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(server *gin.Engine) {
 	server.POST("/students", createStudent)
 	server.GET("/students", getAllStudents)
+	server.POST("/courses", createCourse)
+	server.GET("/courses", getAllCourses)
 }

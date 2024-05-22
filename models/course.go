8933a57cce6b64289b2869dc2f61d00559841ec6
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-22 13:27:12 -0400

    created a function to list all courses

--- models/course.go
+++ models/course.go
@@ -4,17 +4,23 @@ import "github.com/Jeff-Marm-Al/student-registration/db"
 
 type Course struct {
 	CourseID           int64
-	Title              string
-	Ticker             string
-	InstructorLastName string
-	Credits            int64
-	Capacity           int64
+	Title              string `binding:"required"`
+	Ticker             string `binding:"required"`
+	InstructorLastName string `binding:"required"`
+	Credits            int64 `binding:"required"`
+	Capacity           int64 `binding:"required"`
+}
+
+type AllCourses struct {
+	Ticker string
+	Title string
+	Credits int64
 }
 
 // function to insert a new course to the database
 func (new *Course) Save() error {
 	// created sql query
-	query := "INSERT INTO courses(title, ticker, instructor_last_name, capacity) VALUES (?, ?, ?, ?)"
+	query := "INSERT INTO courses(title, ticker, instructor_last_name, credits, capacity) VALUES (?, ?, ?, ?, ?)"
 	statement, err := db.DB.Prepare(query)
 
 	if err != nil {
@@ -23,7 +29,7 @@ func (new *Course) Save() error {
 
 	defer statement.Close()
 
-	statementResult, err := statement.Exec(new.Title, new.Ticker, new.InstructorLastName, new.Capacity)
+	statementResult, err := statement.Exec(new.Title, new.Ticker, new.InstructorLastName, new.Credits, new.Capacity)
 
 	if err != nil {
 		return err
@@ -36,8 +42,31 @@ func (new *Course) Save() error {
 	return err
 }
 
-/*
-func GetAllCourses() {
+// function to query for all courses available
+func GetAllCourses() ([]AllCourses, error) {
+	query := "SELECT ticker, title, credits FROM courses"
+	rows, err := db.DB.Query(query)
 
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var courses []AllCourses
+
+	for rows.Next() {
+		var course AllCourses
+
+		err := rows.Scan(&course.Ticker, &course.Title, &course.Credits)
+
+		if err != nil {
+			return nil, err
+		}
+
+		courses = append(courses, course)
+
+	}
+
+	return courses, nil
 }
-*/

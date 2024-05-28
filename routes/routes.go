93d879144dc3db8d8d52ca7aafbb553f76085652
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-28 10:32:00 -0400

    changed the path for the get student info call

--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(server *gin.Engine) {
 	server.POST("/students", createStudent)
 	server.GET("/students", getAllStudents)
-	server.GET("/students/:firstname/:lastname", getStudentInfo)
+	server.GET("/students/student/:firstname/:lastname", getStudentInfo)
 	server.POST("/courses", createCourse)
 	server.GET("/courses", getAllCourses)
 }

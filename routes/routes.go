879ec6911ce57c428cf88821aa786922fa595bd5
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-28 10:17:47 -0400

    added route for getting a specific student's info

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(server *gin.Engine) {
 	server.POST("/students", createStudent)
 	server.GET("/students", getAllStudents)
+	server.GET("/students/:firstname/:lastname", getStudentInfo)
 	server.POST("/courses", createCourse)
 	server.GET("/courses", getAllCourses)
 }

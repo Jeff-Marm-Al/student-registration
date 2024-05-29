405fb674f2a412d80ea38db500e047777e1d2bf9
Jeff Marmolejos <jeff.marmolejos2@outlook.com>
2024-05-29 11:41:32 -0400

    added route for student PUT call

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ func RegisterRoutes(server *gin.Engine) {
 	server.POST("/students", createStudent)
 	server.GET("/students", getAllStudents)
 	server.GET("/students/student/:firstname/:lastname", getStudentInfo)
+	server.PUT("/students/student/:firstname/:lastname", updateStudentInfo)
 	server.POST("/courses", createCourse)
 	server.GET("/courses", getAllCourses)
 }
